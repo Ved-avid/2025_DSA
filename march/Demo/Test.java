7322b945dc4521664bf92fb2007c07ab461354f9
Ved Singh <64973965+Ved-avid@users.noreply.github.com>
2025-03-20 23:46:16 +0530

    Will be pushing new problems daily after this

---
+++ march/Demo/Test.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class Test {
+    static public void main(String[] args){
+       System.out.println("Today is march 20th, 2025");
+       System.out.println("So from today onwards I will be solving problems daily and pushing the codes to the Github account");
+       System.out.println("Ved Singh...");
+    }
+}
