f90a0e2f77576df9fb853f66c15290afbef5b44b
Ved Singh <64973965+Ved-avid@users.noreply.github.com>
2025-03-25 00:33:57 +0530

    solution added for problem 2 - Remove Elements

--- march/Demo/Solution2.java
+++ march/Demo/Solution2.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Solution2 {
+
+// Problem statement link below
+
+// ---  https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
+
+import java.io.*;
+import java.util.*;
+public class Solution2 {
+    public int removeElement(int[] nums, int val) {
+        int len = nums.length;
+        int N = Integer.MAX_VALUE - 1;
+        int count=0;
+        for(int i=0;i<len;i++){
+            if(nums[i]==val){
+                count++;
+                nums[i]=N;
+            }
+        }
+
+        Arrays.sort(nums);
+
+        return len-count;
+
+    }
 }
