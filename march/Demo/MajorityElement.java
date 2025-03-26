66e90112c5d80579a0390ffad6afafba47cf1bc2
Ved Singh <64973965+Ved-avid@users.noreply.github.com>
2025-03-26 22:05:04 +0530

    DAY 3

--- march/Demo/MajorityElement.java
+++ march/Demo/MajorityElement.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class MajorityElement {
+/*
+*
+* Question link - https://leetcode.com/problems/majority-element/?envType=study-plan-v2&envId=top-interview-150
+*
+* */
+
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int element=0;
+        int count=0;
+
+        for(int num:nums){
+            if(count==0){
+                element=num;
+                count++;
+            }else if(element==num){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return element;
+    }
 }
+
+
+/*
+* Brute force solution
+*
+*     public int majorityElement(int[] nums) {
+       int len=nums.length;
+       int count=0;
+       int sol=0;
+       int max=Integer.MIN_VALUE;
+       for(int i=0;i<len;i++){
+           count=0;
+            for(int j=i+1;j<len;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+            if(max<count){
+                max=count;
+                sol=nums[i];
+            }
+       }
+       return sol;
+    }*/
--- march/Demo/RemoveDuplicatesOnSortedArray.java
+++ march/Demo/RemoveDuplicatesOnSortedArray.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class RemoveDuplicatesOnSortedArray {
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150
+
+import java.util.*;
+import java.io.*;
+public class RemoveDuplicatesOnSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int k=2;
+        int len = nums.length;
+
+        for(int i=2;i<len;i++){
+            if(nums[i]!=nums[k-2]){
+                nums[k]=nums[i];
+                k+=1;
+            }
+        }
+        return k;
+    }
 }
