abe8e542f5380d6fc97de3fd0d93b082e6edf30a
Ved Singh <64973965+Ved-avid@users.noreply.github.com>
2025-03-31 22:18:38 +0530

    DAY-7, Jump Game 2 - accepted solution

--- march/Demo/JumpGame2.java
+++ march/Demo/JumpGame2.java
@@ -1,6 +1,8 @@
 
 // Question link - https://leetcode.com/problems/jump-game-ii/description/?envType=study-plan-v2&envId=top-interview-150
 
+
+import java.util.*;
 public class JumpGame2 {
 
     // This solution gives TLE
@@ -23,6 +25,36 @@ public class JumpGame2 {
             min=Math.min(min,helper(index+i,jumps+1,n));
         }
         return min;
+    }
+
+
+    // This solution works but not very optimised and extra space is used.
+    public int jump2(int[] nums) {
+        int n = nums.length;
+        if(n<=1){
+            return 0;
+        }
+
+        Map<Integer,Integer> map = new HashMap<>();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        map.put(0,0);
+        while(!queue.isEmpty()){
+            int index = queue.poll();
+            int count = map.get(index);
+
+            for(int i = nums[index];i>0;i--){
+                int nextIndex = index+i;
+                if(nextIndex >= n-1){
+                    return count+1;
+                }
+                if(!map.containsKey(nextIndex)){
+                    map.put(nextIndex,count+1);
+                    queue.add(nextIndex);
+                }
+            }
+        }
+        return -1;
     }
 
 }
